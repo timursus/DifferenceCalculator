af6e3e18a25e6083e8757eeec3c0b5d8653abb64
Timursus <redundant09@gmail.com>
2020-04-05 16:25:14 +0300

    add tests for nested data structure

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -3,14 +3,47 @@ import genDiff from '../src/index.js';
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
-const expectedJSON = '{\n    host: hexlet.io\n  + timeout: 20\n  - timeout: 50\n  + verbose: true\n  - proxy: 123.234.53.22\n  - follow: false\n}';
-const expectedYAML = '{\n    node: true\n  + extends: plugin:jest/recommended\n  - extends: airbnb-base\n  + jest: true\n  + parser: babel-eslint\n  - ecmaVersion: 2018\n}';
-const expectedINI = '{\n    StatusPort: 6090\n  + DetailedLog: false\n  - DetailedLog: true\n  + Archive: 1\n  - StatusRefresh: 10\n  - RunStatus: 1\n}';
+const expected = [
+  '{',
+  '    common: {',
+  '      + follow: false',
+  '        setting1: Value 1',
+  '      - setting2: 200',
+  '      - setting3: true',
+  '      + setting3: {',
+  '            key: value',
+  '        }',
+  '        setting6: {',
+  '            key: value',
+  '          + ops: vops',
+  '        }',
+  '      + setting4: blah blah',
+  '      + setting5: {',
+  '            key5: value5',
+  '        }',
+  '    }',
+  '    group1: {',
+  '      + baz: bars',
+  '      - baz: bas',
+  '        foo: bar',
+  '      - nest: {',
+  '            key: value',
+  '        }',
+  '      + nest: str',
+  '    }',
+  '  - group2: {',
+  '        abc: 12345',
+  '    }',
+  '  + group3: {',
+  '        fee: 100500',
+  '    }',
+  '}',
+].join('\n');
 
 test.each([
-  ['before.json', 'after.json', expectedJSON],
-  ['bg.yml', 'gd.yml', expectedYAML],
-  ['config.ini', 'modified.ini', expectedINI],
+  ['before.json', 'after.json', expected],
+  ['before.yml', 'after.yml', expected],
+  ['before.ini', 'after.ini', expected],
 ])('Comparsion of files %p %p', (configA, configB, expectedOutput) => {
   const pathA = getFixturePath(configA);
   const pathB = getFixturePath(configB);

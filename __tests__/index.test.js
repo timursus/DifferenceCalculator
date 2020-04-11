8ca946e76c7e322d57f38e6fbbba99d01d4aacca
Timursus <redundant09@gmail.com>
2020-04-11 12:47:36 +0300

    setup with 'beforeAll'

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -4,21 +4,27 @@ import mainFlow from '../src/index.js';
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
-const prettyOutput = readFileSync(getFixturePath('pretty.txt'), 'utf-8');
-const plainOutput = readFileSync(getFixturePath('plain.txt'), 'utf-8');
-const jsonOutput = readFileSync(getFixturePath('json.txt'), 'utf-8');
+let prettyOutput;
+let plainOutput;
+let jsonOutput;
+
+beforeAll(() => {
+  prettyOutput = readFileSync(getFixturePath('pretty.txt'), 'utf-8');
+  plainOutput = readFileSync(getFixturePath('plain.txt'), 'utf-8');
+  jsonOutput = readFileSync(getFixturePath('json.txt'), 'utf-8');
+});
 
 test.each([
-  ['before.json', 'after.json', 'pretty', prettyOutput],
-  ['before.json', 'after.json', 'plain', plainOutput],
-  ['before.json', 'after.json', 'json', jsonOutput],
-  ['before.yml', 'after.yml', 'pretty', prettyOutput],
-  ['before.ini', 'after.ini', 'pretty', prettyOutput],
-])('Comparsion %p & %p, output format: %s', (configA, configB, format, expected) => {
+  ['before.json', 'after.json'],
+  ['before.yml', 'after.yml'],
+  ['before.ini', 'after.ini'],
+])('Comparsion %p & %p', (configA, configB) => {
   const pathA = getFixturePath(configA);
   const pathB = getFixturePath(configB);
 
-  expect(mainFlow(pathA, pathB, format)).toBe(expected);
+  expect(mainFlow(pathA, pathB, 'pretty')).toBe(prettyOutput);
+  expect(mainFlow(pathA, pathB, 'plain')).toBe(plainOutput);
+  expect(mainFlow(pathA, pathB, 'json')).toBe(jsonOutput);
 });
 
 test('Throws on incorrect input', () => {

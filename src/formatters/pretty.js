fc909543908662d1eaf00de9dc89b0e85d442e0e
Timursus <redundant09@gmail.com>
2020-12-08 12:58:22 +0300

    move stringify out of render

--- src/formatters/pretty.js
+++ src/formatters/pretty.js
@@ -3,28 +3,27 @@ import { isPlainObject } from 'lodash';
 const indentUnit = '    ';
 const generateIndent = (depth) => `\n${indentUnit.repeat(depth)}`;
 
+const stringify = (data, depth, render) => {
+  if (isPlainObject(data)) {
+    const normalizedObj = Object.entries(data)
+      .map(([key, value]) => ({ type: 'unchanged', key, value }));
+    return render(normalizedObj, depth + 1);
+  }
+  return data?.toString();
+};
+
 const render = (diff, depth) => {
-  const stringify = (data) => {
-    if (isPlainObject(data)) {
-      const normalizedObj = Object.entries(data)
-        .map(([key, value]) => ({ type: 'unchanged', key, value }));
-      return render(normalizedObj, depth + 1);
-    }
-    return data?.toString();
-  };
-
   const indent = generateIndent(depth);
-
   const lines = diff.map(({
     type, key, value, valueOld, children,
   }) => {
-    const strValue = stringify(value);
+    const strValue = stringify(value, depth, render);
 
     switch (type) {
       case 'unchanged':
         return `    ${key}: ${strValue}`;
       case 'changed':
-        return `  - ${key}: ${stringify(valueOld)}${indent}  + ${key}: ${strValue}`;
+        return `  - ${key}: ${stringify(valueOld, depth, render)}${indent}  + ${key}: ${strValue}`;
       case 'added':
         return `  + ${key}: ${strValue}`;
       case 'deleted':

d099b08767c087d460135e3eb3be193fbb291f5e
Timursus <redundant09@gmail.com>
2020-12-08 18:39:31 +0300

    small prepublish edits

--- babel.config.js
+++ babel.config.js
@@ -3,7 +3,7 @@ module.exports = {
     ['@babel/preset-env',
       {
         targets: {
-          node: 'current',
+          node: '13',
         },
       },
     ],
--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -2,14 +2,15 @@
 
 import program from 'commander';
 import gendiff from '../index.js';
+import { version } from '../../package.json';
 
 program
-  .version('0.3.1')
+  .version(version)
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format <type>', 'output format [pretty, plain, json]', 'pretty')
-  .arguments('<firstConfig> <secondConfig>')
-  .action((firstConfig, secondConfig) => {
-    console.log(gendiff(firstConfig, secondConfig, program.format));
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2) => {
+    console.log(gendiff(filepath1, filepath2, program.format));
   });
 
 program.parse(process.argv);

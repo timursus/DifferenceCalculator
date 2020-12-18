3783185506d0901de47942faef66e49bb4ced39f
Timursus <redundant09@gmail.com>
2020-12-18 14:04:42 +0300

    emulate __dirname

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,9 +1,13 @@
 import { test, expect, beforeAll } from '@jest/globals';
+import { fileURLToPath } from 'url';
 import path from 'path';
 import { readFileSync } from 'fs';
 import mainFlow from '../src/index.js';
 
-const getFixturePath = (filename) => path.resolve('__fixtures__', filename);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
 let prettyOutput;
 let plainOutput;
